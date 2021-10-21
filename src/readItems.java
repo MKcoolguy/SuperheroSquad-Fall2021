5f8ff841d2b61419ce3696017cd84b131fdaef72
eengida <eengida@ggc.edu>
2021-10-21 11:37:00 -0400

    reading the item txt

---
+++ src/readItems.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class readItems {
+
+	public static void main(String[] args) throws FileNotFoundException
+	
+	{
+		
+		Scanner reader = new Scanner(new File("src//group-item.txt"));
+		
+		while(reader.hasNext()) {
+			
+			String singleItem = reader.nextLine();
+			String[] itemsArray = singleItem.split("-");
+			int itemRoom = Integer.parseInt(itemsArray[0]);
+			String itemName = itemsArray[1];
+			String itemDes = itemsArray[2];
+			String itemType =  itemsArray[3];
+			String itemPower = itemsArray[4];
+			
+			
+		
+		
+		
+		
+		
+
+	}
+
+}
+}
