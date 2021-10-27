5045336dcab9f5159334a5cea3d3f4cec69806d0
eengida <92343022+eengida@users.noreply.github.com>
2021-10-27 14:15:13 -0400

    Update UserInterface.java

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,1 +1,32 @@
 
+public class Ui {
+
+	
+	
+	String message;
+	String title;
+	public Ui() {     //constructor
+		
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+    public String displayedElements() {
+    	return  "";
+    }
+		
+		
+		
+	
+
+}
+
