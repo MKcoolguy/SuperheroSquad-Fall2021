06e6cd46b1feb2f6315e1e72124fc7c6a859450f
Matteo <matteokitic7@gmail.com>
2021-10-28 19:16:25 -0400

    Refactor and code clean up

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,32 +1,30 @@
+public class UserInterface {
 
-public class Ui {
+    private String message;
+    private String tile;
 
-	
-	
-	String message;
-	String title;
-	public Ui() {     //constructor
-		
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-    public String displayedElements() {
-    	return  "";
+
+        public UserInterface() {     //constructor
+
+        }
+
+    public String getMessage() {
+        return message;
     }
-		
-		
-		
-	
 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTile() {
+        return tile;
+    }
+
+    public void setTile(String tile) {
+        this.tile = tile;
+    }
+
+    public String displayedElements() {
+        return  "";
+    }
 }
-
