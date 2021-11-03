1bf8c6ae391488e600e2f4c59d42153dd6ec586f
eengida <92343022+eengida@users.noreply.github.com>
2021-11-03 19:00:19 -0400

    Update Monster.java
    update monster class

--- src/Monster.java
+++ src/Monster.java
@@ -1,10 +1,16 @@
 import java.io.Serializable;
 
-public class Monster implements Serializable {
+public class Monster extends Entity implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public int dealDamage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
