79f600f70bf51e41424cbf54ef6be42dfb60ff90
eengida <eengida@ggc.edu>
2021-11-05 22:34:01 -0400

    monster class attributes

--- src/Monster.java
+++ src/Monster.java
@@ -8,6 +8,25 @@ public class Monster extends Entity implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	String monsterAttack;   //Butter-butter bodyslam
+	String monsterRewardCarried;  //HP or attack potion
+
+	public String getMonsterAttack() {
+		return monsterAttack;
+	}
+
+	public void setMonsterAttack(String monsterAttack) {
+		this.monsterAttack = monsterAttack;
+	}
+
+	public String getMonsterRewardCarried() {
+		return monsterRewardCarried;
+	}
+
+	public void setMonsterRewardCarried(String monsterRewardCarried) {
+		this.monsterRewardCarried = monsterRewardCarried;
+	}
 
 	public Monster(int id, String name, int health, int healthMax, String desc, int stength, int entityLocation, HashMap<String, Queue<GameItem>> inventory) {
 		super(id, name, health, healthMax, desc, stength, entityLocation, inventory);
